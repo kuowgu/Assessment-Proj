fa73b3514a96dc0f9845aa52e08167baeaa853a1
Kuo on Fedora <liangkuo28@gmail.com>
2022-02-15 17:31:18 -0500

    create properties file

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,27 +1,35 @@
-package sample;
+package main;
 
+import dataAccess.JDBC;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Main extends Application {
 
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/LoginView.fxml"));
+        stage.setTitle("Login page");
+        stage.setScene(new Scene(root, 600, 400));
+        stage.show();
     }
 
 
     public static void main(String[] args) {
+
+        Locale.setDefault(new Locale("fr"));
+
+        ResourceBundle rb = ResourceBundle.getBundle("source/Nat", Locale.getDefault());
+        System.out.println(rb.getString("hello"));
+
         JDBC.makeConnection();
-        System.out.println("connection made");
-	System.out.println("launching gui");
-	//TODO there should code to set system to French.
         launch(args);
     }
 }
