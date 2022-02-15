40474e70012aa70af062dc5644f352ae07715f79
Kuo on Fedora <liangkuo28@gmail.com>
2022-02-15 17:34:11 -0500

    login view add

---
+++ src/Controller/LoginViewController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginViewController implements Initializable {
+    public Label loginLabel;
+    public Label passwordLabel;
+    public TextField loginTxtField;
+    public TextField passwordTxtField;
+    public Button loginButton;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void onLogin(ActionEvent actionEvent) {
+    }
+}
